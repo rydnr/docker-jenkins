1bc4489830d0a351c139afabb32e67ff4f84b547
chous <chous@acm-sl.org>
2014-07-05 11:34:39 +0200

    Fixed war path

--- jenkins.sh
+++ jenkins.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec /sbin/setuser jenkins /usr/bin/java -jar jenkins.war >> /var/log/jenkins.log 2>&1
+exec /sbin/setuser jenkins /usr/bin/java -jar /opt/jenkins.war >> /var/log/jenkins.log 2>&1
